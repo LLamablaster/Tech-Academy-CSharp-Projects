7bc1567f29e064d0f5a5847d9cfd00920016a67b
Sam Engen <sam.p.engen@gmail.com>
2023-07-05 12:13:49 -0700

    added boolean logic console app

---
+++ Boolean/Boolean/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? (please enter true or false)");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets have you had?");
+            int tickets = Convert.ToInt16(Console.ReadLine());
+
+            if (age > 15 && dui == false && tickets <= 3)
+            {
+                Console.WriteLine("Congratulations! You qualify for a licensing test.");
+            } else {
+                Console.WriteLine("Unfortunately you do not qualify for a license at this time."); 
+            }
+            Console.ReadLine();
+        }
+    }
+}
