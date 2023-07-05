3621794d0c64156740321544e0b50ea27d4bd81b
Sam Engen <sam.p.engen@gmail.com>
2023-07-05 11:44:12 -0700

    added hello world and daily report console programs

---
+++ Program.cs/Program.cs/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy.");
+            Console.WriteLine("Student Daily Report.");
+
+            Console.WriteLine("What is your name?");
+            string yourName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + yourName +". What course are you in?");
+            string yourCourse = Console.ReadLine();
+
+            Console.WriteLine("What page number? (Please enter numerals)");
+            int yourPage = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Do you need help with anything? (Please enter 'true' or 'false')");
+            bool yourHelp = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            string yourPositives = Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string yourFeedback = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            int yourStudyTime = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Thank you for your answers.");
+            Console.WriteLine("An Instructor will respond to this shortly.");
+            Console.WriteLine("Have a great day!");
+
+            Console.ReadLine();
+        }
+    }
+}
