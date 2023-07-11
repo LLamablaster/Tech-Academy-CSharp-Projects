c52e2f4c5341d961685af9d09c59d31958d33f2a
Sam Engen <sam.p.engen@gmail.com>
2023-07-11 11:18:15 -0700

    while loop console app added

---
+++ While/While/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            while (x<4) {
+                x++;
+                Console.WriteLine("While Iteration " + x);
+                if (x%2 == 0)
+                {
+                    Console.WriteLine("This iteration is even.");
+                } 
+            }
+
+            int y = 0;
+            do
+            {
+                y++;
+                Console.WriteLine("Do While Iteration " + y);
+                if (y % 2 == 0)
+                {
+                    Console.WriteLine("This iteration is even.");
+                }
+            }
+            while (y < 4);
+
+            Console.ReadLine();
+        }
+    }
+}
