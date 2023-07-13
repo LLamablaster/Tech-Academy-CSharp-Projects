de3805321a72ad6f50f2d2b3cc252a686dedb14c
Sam Engen <sam.p.engen@gmail.com>
2023-07-13 15:09:00 -0700

    exceptions console app added

---
+++ Exceptions/Exceptions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int> { 10, 20, 30, 40, 50 };
+            try
+            {
+                Console.WriteLine("please enter a number to divide by");
+                double div = Convert.ToDouble(Console.ReadLine());
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    Console.WriteLine(numbers[i] + " divided by " + div + " is " + numbers[i] / div);
+                }
+                Console.ReadLine();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter a number comprised of digits.");
+                Console.ReadLine();
+
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Please enter a nonzero number.");
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
