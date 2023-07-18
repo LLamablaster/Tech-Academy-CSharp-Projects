ed2f0195584eeeb40eeb321c1dea7249a66dbd91
Sam Engen <sam.p.engen@gmail.com>
2023-07-18 15:34:53 -0700

    adjusted console app assignment

--- Main/Main/Maffs.cs
+++ Main/Main/Maffs.cs
@@ -8,20 +8,20 @@ namespace Main
 {
     class Maffs
     {
-        public int multiply(int input)
+        public int maff(int input)
         {
             Console.WriteLine("Multiplying {0} by 5.", input);
             return input * 5;
         }
 
-        public int divide(double input)
+        public int maff(decimal input)
         {
             Console.WriteLine("Dviding {0} by 2.", input);
-            int var = Convert.ToInt32(input);
-            return var / 2;
+            decimal var = Convert.ToDecimal(input);
+            return Convert.ToInt32((var / Convert.ToDecimal(2)));
         }
 
-        public int add(string input)
+        public int maff(string input)
         {
             Console.WriteLine("Adding {0} to 3.", input);
             int var = Convert.ToInt32(input);
--- Main/Main/Program.cs
+++ Main/Main/Program.cs
@@ -13,15 +13,15 @@ namespace Main
             Maffs maffs = new Maffs();
 
             Console.WriteLine("Please enter an integer.");
-            int answer1 = maffs.multiply(Convert.ToInt32(Console.ReadLine()));
+            int answer1 = maffs.maff(Convert.ToInt32(Console.ReadLine()));
             Console.WriteLine("Answer was {0}.", answer1);
 
             Console.WriteLine("Please enter a decimal.");
-            int answer2 = maffs.divide(Convert.ToDouble(Console.ReadLine()));
+            int answer2 = maffs.maff(Convert.ToDecimal(Console.ReadLine()));
             Console.WriteLine("Answer was around {0}.", answer2);
 
             Console.WriteLine("Please enter an integer.");
-            int answer3 = maffs.add(Console.ReadLine());
+            int answer3 = maffs.maff(Console.ReadLine());
             Console.WriteLine("Answer was {0}.", answer3);
             Console.ReadLine();
         }
