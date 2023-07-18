a457377fca8432d6960b1e18338c8b97bb2a6adf
Sam Engen <sam.p.engen@gmail.com>
2023-07-18 13:11:57 -0700

    added optional parameters console app

---
+++ OptionalParamaters/OptionalParamaters/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParamaters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            add add = new add();
+
+            Console.WriteLine("Please enter first integer to add.");
+            int xx = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("(OPTIONAL) Please enter second integer to add.");
+            string y = Console.ReadLine();
+            if (y != "")
+            {
+                int yy = Convert.ToInt32(y);
+                Console.WriteLine("Sum of your numbers is {0}", add.addition(xx, yy));
+            } else
+            {
+                Console.WriteLine("Sum of your numbers is {0}", add.addition(xx));
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OptionalParamaters/OptionalParamaters/add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParamaters
+{
+    class add
+    {
+        public int addition(int x, int y = 0)
+        {
+            return x + y;
+        }
+    }
+}
