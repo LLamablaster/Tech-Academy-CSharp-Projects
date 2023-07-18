5a1c021597918ee64806c932c318c2e4fedf94e3
Sam Engen <sam.p.engen@gmail.com>
2023-07-18 15:13:41 -0700

    added named parameter console app

---
+++ VoidMethod/VoidMethod/Pitcher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Pitcher
+    {
+        public void pour(int x, int y)
+        {
+            int z = x * 2;
+            Console.WriteLine(y);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pitcher pitcher = new Pitcher();
+            pitcher.pour(1, 2);
+
+            pitcher.pour(y:2, x:1);
+        }
+    }
+}
