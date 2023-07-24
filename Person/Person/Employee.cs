89e0ca88d9921e2e455421a82267e40b3b8c6195
Sam Engen <sam.p.engen@gmail.com>
2023-07-24 11:26:57 -0700

    added person

---
+++ Person/Person/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    class Employee : Person
+    {
+        public Employee(int id = 0, string f = "Jane", string l = "Doe") : base(f,l)
+        {
+            Id = id;
+        }
+
+        public int Id { get; set; }
+
+    }
+}
